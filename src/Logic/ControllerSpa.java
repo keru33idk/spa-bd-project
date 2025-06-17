227c923fecdd264fad20d868ea9d92bc96b084bb
Kel <kerulionhart@gmail.com>
2025-06-16 20:55:39 -0700

    acceso Base de Datos

--- src/Logic/ControllerSpa.java
+++ src/Logic/ControllerSpa.java
@@ -1,4 +1,22 @@
 package Logic;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ControllerSpa {
+    private final String url = "jdbc:postgresql://localhost:1234/SPA";
+    private final String user = "postgres";
+    private final String password = "12345678";
+
+    public Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Conectado a PostgreSQL correctamente");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import Logic.ControllerSpa;
+
+
+
+
 public class Main {
+
     public static void main(String[] args) {
+        ControllerSpa app = new ControllerSpa();
+        app.connect();
         System.out.println("Hello world!");
     }
 }
